98e163858bd5954d5bac2750017c5c743f0540bd
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-02-25 19:41:06 +0000

    fix: same as before, but now actually fixed

--- scripts/copy-system-files.sh
+++ scripts/copy-system-files.sh
@@ -8,10 +8,11 @@ if [[ -d "$SYSTEM_FILES_DIR" ]]; then
         exit 1
     }
     
-    cp -r "$SYSTEM_FILES_DIR"/etc/skel/* /root || {
+    cp -r "$SYSTEM_FILES_DIR"/etc/skel/.* /root/ || {
 	echo "Failed to copy configs to root user"
     	exit 1
     }
+    ls 
 else
     echo "Warning: System files directory not found"
 fi
