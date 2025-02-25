d9d21d409a6c36486902eb795f894d70ec14a376
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-02-25 23:26:08 +0000

    feat: add script to configure root user on first login

--- scripts/configure-dotfiles.sh
+++ scripts/configure-dotfiles.sh
@@ -8,14 +8,55 @@ git clone https://github.com/reisaraujo-miguel/my-dot-files.git
 
 cd my-dot-files || exit
 
-# configure system wide
-cp -r .zshenv /etc/zshenv || exit
-cp -r .gitconfig /etc/gitconfig || exit
-cp -r .config/* /etc || exit
+ignore_files=("LICENSE" "README.md" ".git" "install.sh")
 
 # configure new users
 mkdir -p /etc/skel
 
-cp -r .zshenv .gitconfig .config /etc/skel || exit
+for file in *; do
+	ignore=false
+ 	for ignored_file in "${ignore_files[@]}"; do
+    		if [[ "$file" == "./$ignored_file" ]]; then
+      			ignore=true
+      			break
+    		fi
+  	done
+
+  	if [[ "$ignore" == false && "$file" != "$destination_dir" ]]; then
+    		cp -r "$file" /etc/skel
+  	fi
+done
+
+# Configure root user on first login
+echo "
+# checking if I am root
+[ \"\$(id -u)\" -eq 0 ] || exit
+
+# creating root dir if it does not exists
+[ ! -d /var/roothome ] && mkdir -p /var/roothome
+
+# checking if this script has already been executed once
+[ -f /root/.setuplock ] && exit
+
+ignore_files=(\"LICENSE\" \"README.md\" \".git\" \"install.sh\")
+
+# cloning all non ignored files
+for file in *; do
+	ignore=false
+        for ignored_file in \"\${ignore_files[@]}\"; do
+                if [[ \"\$file\" == \"./\$ignored_file\" ]]; then
+                        ignore=true
+                        break
+                fi
+        done
+
+        if [[ \"\$ignore\" == false && \"\$file\" != \"\$destination_dir\" ]]; then
+                cp -r \"\$file\" /root
+        fi
+done
+
+# preventing script from running again
+touch /root/.setuplock
+" > /etc/profile.d/setup-root.sh	
 
 cd / || exit
