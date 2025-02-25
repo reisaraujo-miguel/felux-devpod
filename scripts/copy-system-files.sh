7c20bd7ff830cf8e41197c2da9bfedfda25583ab
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-02-25 19:27:13 +0000

    fix: add double quotes to enable gobblig

--- scripts/copy-system-files.sh
+++ scripts/copy-system-files.sh
@@ -3,12 +3,12 @@
 set -ouex pipefail
 
 if [[ -d "$SYSTEM_FILES_DIR" ]]; then
-    cp -r "$SYSTEM_FILES_DIR"/* / || {
+    cp -r "$SYSTEM_FILES_DIR/*" / || {
         echo "Failed to copy system files"
         exit 1
     }
     
-    cp -r "$SYSTEM_FILES_DIR"/etc/skel/* /root/ || {
+    cp -r "$SYSTEM_FILES_DIR/etc/skel/.*" /root/ || {
 	echo "Failed to copy configs to root user"
     	exit 1
     }
